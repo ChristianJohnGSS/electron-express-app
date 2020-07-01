f980bbfdcc7c8286a2436a4ee07140113a0f4409
christianjohn.gss <christianjohn.gss@gmail.com>
2020-07-01 16:25:38 +0800

    Add CRUD routes

--- routes/users.js
+++ routes/users.js
@@ -22,4 +22,44 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.post('/users/store', (req, res) => {
+
+    const { username, password } = { ...req.body };
+
+    const SQL = `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`;
+    mysql.query(SQL, (error, result, fields) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log(result)
+        res.json("User created!");
+    });
+});
+
+router.delete('/users/delete', (req, res) => {
+
+    const SQL = `DELETE FROM users WHERE username = '${req.body.username}'`;
+    mysql.query(SQL, (error, result, fields) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log(result)
+        res.json("User deleted!");
+    });
+});
+
+router.put('/users/update', (req, res) => {
+
+    const { username, password } = { ...req.body };
+
+    const SQL = `UPDATE users SET password = '${password}' WHERE username = '${username}`;
+    mysql.query(SQL, (error, result, fields) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log(result)
+        res.json("User updated!");
+    });
+});
+
 module.exports = router;
